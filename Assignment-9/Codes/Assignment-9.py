83a4ea2bc15952c59d02cf5090fa37d632a61074
SATYA SANGRAM MISHRA <58678558+satyasm45@users.noreply.github.com>
2021-06-23 10:11:31 +0530

    Update Assignment-9.py

--- Assignment-9/Codes/Assignment-9.py
+++ Assignment-9/Codes/Assignment-9.py
@@ -8,66 +8,83 @@ Original file is located at
 """
 
 #Code by Satya Sangram
-#For Inequality 2.67
-import numpy as np
+#For Inequality 2.56
 import matplotlib.pyplot as plt
 from coeffs import *
+import numpy as np
 
-#vertices
-A = np.array([-15, 0])
-B = np.array([15, 0])
-C = np.array([-5, 0])
-D = np.array([0,0])
+affine = np.array(([-1,2],[3,4]))
+c =  np.array([-3,12])
+
+#Original axes
+points = np.array([[0, 0], [0, 9],[9, 0]])
+
+#Transformed axes
+affine_points = np.linalg.inv(affine)@(c+points).T
+affine_points = affine_points.T
 
 plt.figure(0)
-#Generating and plotting points on number line
-X_coord = np.array(np.linspace(-14, 14, num=29))
-Y_coord = np.zeros(29)
-plt.plot(X_coord, Y_coord, '|',color='k')
+#Filling up the desired region
+plt.fill(affine_points[:,0], affine_points[:,1], 'k', alpha=0.3,label="pair1")
 
-#Generating all lines
-x_AB = line_gen(A,B)
-x_CB = line_gen(C,B)
-
-#Plotting all lines
-plt.plot(x_AB[0,:], x_AB[1,:], color='k')
-plt.plot(x_CB[0,:], x_CB[1,:], color='r',linewidth=2.5, label='x>-5')
-
-#Plotting and labelling points
-plt.plot(A[0], A[1], '<',color='k')
-plt.text(A[0]-1, 0.005, '$-∞$')
-plt.plot(B[0], B[1], '>',color='r')
-plt.text(B[0], 0.005, '$∞$')
-plt.plot(C[0], C[1], 'o',color='b')
-plt.text(C[0]-0.5, 0.005 , '$-5$')
-plt.text(D[0]-0.2, 0.005 , '$0$')
-
-plt.axis('off')
+plt.xlabel('$x$');plt.ylabel('$y$')
 plt.legend(loc='best')
-plt.show()
+plt.grid()
+
+
+affine = np.array(([1,0],[0,1]))
+c =  np.array([0,1])
+
+#Original axes
+points = np.array([[0, 0], [0, 5],[5,5 ],[5,0]])
+
+#Transformed axes
+affine_points = np.linalg.inv(affine)@(c+points).T
+affine_points = affine_points.T
 
 plt.figure(1)
-C = np.array([5, 0])
-x_CB = line_gen(C,B)
+#Filling up the desired region
+plt.fill(affine_points[:,0], affine_points[:,1], 'r', alpha=0.3,label="pair2")
 
-#Generating and plotting points on number line
-X_coord = np.array(np.linspace(-14, 14, num=29))
-Y_coord = np.zeros(29)
-plt.plot(X_coord, Y_coord, '|',color='k')
-
-#Plotting all lines
-plt.plot(x_AB[0,:], x_AB[1,:], color='k')
-plt.plot(x_CB[0,:], x_CB[1,:], color='r',linewidth=2.5, label='x>5')
-
-#Plotting and labelling points
-plt.plot(A[0], A[1], '<',color='k')
-plt.text(A[0]-1, 0.005, '$-∞$')
-plt.plot(B[0], B[1], '>',color='r')
-plt.text(B[0], 0.005, '$∞$')
-plt.plot(C[0], C[1], 'o',color='b')
-plt.text(C[0]-0.5, 0.005 , '$5$')
-plt.text(D[0]-0.2, 0.005 , '$0$')
-
-plt.axis('off')
+#show plot
+plt.xlabel('$x$');plt.ylabel('$y$')
 plt.legend(loc='best')
+plt.grid()
+
+plt.figure(2)
+affine = np.array(([-1,2],[3,4]))
+c =  np.array([-3,12])
+
+#Original axes
+points = np.array([[0, 0], [0, 15],[15, 0]])
+
+#Transformed axes
+affine_points = np.linalg.inv(affine)@(c+points).T
+affine_points = affine_points.T
+
+
+#Filling up the desired region
+plt.fill(affine_points[:,0], affine_points[:,1], 'k', alpha=0.3,label="pair1")
+
+affine = np.array(([1,0],[0,1]))
+c =  np.array([0,1])
+
+#Original axes
+points = np.array([[0, 0], [0, 6],[6,6 ],[6,0]])
+
+#Transformed axes
+affine_points = np.linalg.inv(affine)@(c+points).T
+affine_points = affine_points.T
+
+#Filling up the desired region
+plt.fill(affine_points[:,0], affine_points[:,1], 'r', alpha=0.3,label="pair2")
+
+#show plot
+plt.xlabel('$x$');plt.ylabel('$y$')
+plt.legend(loc='best')
+plt.grid()
+
+
+
+
 plt.show()
